4d61028e3a54998a80af8e6ed01b0bb62dc41bc0
Chad Schroeder <gmchad@gmail.com>
2019-01-17 09:23:54 -0800

    removed flex from Company wrapper

--- frontend/src/Company/index.js
+++ frontend/src/Company/index.js
@@ -8,20 +8,18 @@ class Company extends Component {
   }
   render() {
     return (
-      <div className="company">
-        <div className="card shadow-sm mb-3">
-          <div className="card-body">
-            <div className="media">
-              <img
-                src="images/1.svg"
-                className="mr-3"
-                width="50"
-                alt="corporate logo"
-              />
-              <div className="media-body">
-                <h5 className="card-title">{this.props.detail.name}</h5>
-                <p className="card-text">{this.props.detail.description}</p>
-              </div>
+      <div className="card shadow-sm mb-3">
+        <div className="card-body">
+          <div className="media">
+            <img
+              src="images/1.svg"
+              className="mr-3"
+              width="46"
+              alt="corporate logo"
+            />
+            <div className="media-body">
+              <h5 className="card-title">{this.props.detail.name}</h5>
+              <p className="card-text">{this.props.detail.description}</p>
             </div>
           </div>
         </div>
