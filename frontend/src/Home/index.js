f46fc872f2472d562e3c8c342457b329c3375c18
Chad Schroeder <gmchad@gmail.com>
2019-01-18 14:38:33 -0800

    Updating Home to include login welcome

--- frontend/src/Home/index.js
+++ frontend/src/Home/index.js
@@ -6,7 +6,12 @@ class Home extends Component {
     return (
       <div className="Home">
         <h1>Jobly</h1>
-        <p className="lead">All the jobs in one, convenient place.</p>
+        <p className="lead mb-1">All the jobs in one, convenient place.</p>
+        {this.props.currentUser && (
+          <p className="Home-welcome-back h3 m-0">
+            Welcome back, {this.props.currentUser.first_name}!
+          </p>
+        )}
       </div>
     );
   }
