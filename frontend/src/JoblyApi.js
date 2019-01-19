893d96ec79a33b9386ab4cff45a19088a9b743b6
Chad Schroeder <gmchad@gmail.com>
2019-01-18 16:02:44 -0800

    Updated JoblyApi

--- frontend/src/JoblyApi.js
+++ frontend/src/JoblyApi.js
@@ -15,14 +15,13 @@ class JoblyApi {
     try {
       return (await axios({
         method: verb,
-        url: `http://localhost:3001/${endpoint}`,
+        url: `${BASE_URL}/${endpoint}`,
         [verb === 'get' ? 'params' : 'data']: paramsOrData
       })).data;
       // axios sends query string data via the "params" key,
       // and request body data via the "data" key,
       // so the key we need depends on the HTTP verb
     } catch (err) {
-      console.error('API Error 2:', err.response);
       let message = err.response.data.message;
       throw Array.isArray(message) ? message : [message];
     }
